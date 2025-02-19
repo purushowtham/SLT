c2a7d14b897805f36c19f1fef5e790a17cfe6413
purushowtham <bandipurushowthamsai0469@gmail.com>
2025-02-19 16:43:15 +0530

    data

--- coll_img.py
+++ coll_img.py
@@ -10,7 +10,7 @@ number_of_classes = 3
 dataset_size = 100
 
 cap = cv2.VideoCapture(0)
-for j in range(number_of_clases):
+for j in range(number_of_classes):
     if not os.path.exists(os.path.join(DATA_DIR, str(j))):
         os.makedirs(os.path.join(DATA_DIR, str(j)))
 
